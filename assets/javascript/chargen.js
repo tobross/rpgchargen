c0db3fcb87897eb6e13f359282bb6b7e71cf6563
Tobey Ross <tobross@gmail.com>
2018-04-24 17:12:41 -0500

    added proper links to BS and jquery. Added dropdown functionality.

---
+++ assets/javascript/chargen.js
@@ -0,0 +1,9 @@
+
+var stats = ["#str", "#dex", "#con", "#int", "#wis", "#cha"];
+$(".stats").on("click", "#randomizer", function() {
+    // for (i = 0; i < stats.length; i++) {
+    //     return Math.floor(Math.random() * 18) +4;
+    //     $(".stats").html("<li id='str'>Strength: " + stats[0] + "</li><li id='dex'>Dexterity: " + stats[1] + "</li><li id='con'>Constitution: " + stats[2] + "</li><li id='int'>Intelligence: " + stats[3] + "</li><li id='wis'>Wisdom: " + stats[4] + "</li><li id='cha'>Charisma: " + stats[5] + "</li>");
+        console.log(stats);
+    // }
+})

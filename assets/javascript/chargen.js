a00b9e334e4a80afc0be1dcc6c364954ca33d9d3
Tobey Ross <tobross@gmail.com>
2018-05-02 17:27:18 -0500

    "added new character images"

--- assets/javascript/chargen.js
+++ assets/javascript/chargen.js
@@ -1,9 +1,12 @@
 
-var stats = ["#str", "#dex", "#con", "#int", "#wis", "#cha"];
-$(".stats").on("click", "#randomizer", function() {
-    // for (i = 0; i < stats.length; i++) {
-    //     return Math.floor(Math.random() * 18) +4;
-    //     $(".stats").html("<li id='str'>Strength: " + stats[0] + "</li><li id='dex'>Dexterity: " + stats[1] + "</li><li id='con'>Constitution: " + stats[2] + "</li><li id='int'>Intelligence: " + stats[3] + "</li><li id='wis'>Wisdom: " + stats[4] + "</li><li id='cha'>Charisma: " + stats[5] + "</li>");
-        console.log(stats);
-    // }
+$(document).ready(function() {
+    $(".nameForm").removeClass("hide");
+$(".nameBtn").on("click", function() {
+    $(".nameForm").addClass("hide");
+    $(".pickRole").removeClass("hide");
 })
+$(".")
+// User names their character.
+
+//user selects a role from dropdown menu
+});
